4b87a6534447789c4215eb68c7c0de8474e308a0
Charlie Baker <charles.d.baker.24@dartmouth.edu>
2024-02-13 16:23:19 -0500

    all of texture -- not just in react app

--- python-files/process_midi_files.py
+++ python-files/process_midi_files.py
@@ -21,7 +21,8 @@ def midi_to_notes(file_path):
 
 def main():
   # Replace 'your_file.mid' with the path to your MIDI file
-  midi_file_path = 'chopin/chp_op18.mid'
+  # midi_file_path = '../algorithmic-analysis-classical-music/src/composers/baroque/bach/bach_847.mid'
+  midi_file_path = '../algorithmic-analysis-classical-music/src/composers/classical/beethoven/appass_1.mid'
   notes = midi_to_notes(midi_file_path)
   
   channels = []
@@ -64,28 +65,28 @@ def main():
         #     tempo_bpm = int(mido.tempo2bpm(msg.tempo))
         #     print(f"Tempo: {tempo_bpm} BPM set at time {msg.time}" )
 
-  print("\n-------------------------------------\n")
-  print("Note information on piece itself")
-  print("\n-------------------------------------\n")
-  print("channel 1 had " + str(count_1) + " notes in it")
-  print("these are all of the notes that appeared in channel 1")
-  print(notes_1)
-  print(len(notes_1))
-  print("channel 2 had " + str(count_2) + " notes in it")
-  print("these are all of the notes that appeared in channel 2")
-  print(notes_2)
-  print(len(notes_2))
+  # print("\n-------------------------------------\n")
+  # print("Note information on piece itself")
+  # print("\n-------------------------------------\n")
+  # print("channel 1 had " + str(count_1) + " notes in it")
+  # print("these are all of the notes that appeared in channel 1")
+  # print(notes_1)
+  # print(len(notes_1))
+  # print("channel 2 had " + str(count_2) + " notes in it")
+  # print("these are all of the notes that appeared in channel 2")
+  # print(notes_2)
+  # print(len(notes_2))
 
-  # Using pretty midi package for note analysis
-  midi_data = pretty_midi.PrettyMIDI(midi_file_path)
+  # # Using pretty midi package for note analysis
+  # midi_data = pretty_midi.PrettyMIDI(midi_file_path)
 
-  # Get chroma matrix
-  chroma_matrix = midi_data.get_chroma()
-  print(chroma_matrix)
+  # # Get chroma matrix
+  # chroma_matrix = midi_data.get_chroma()
+  # print(chroma_matrix)
 
-  # Get pitch transition matrix
-  matrix = midi_data.get_pitch_class_transition_matrix()
-  print(matrix)
+  # # Get pitch transition matrix
+  # matrix = midi_data.get_pitch_class_transition_matrix()
+  # print(matrix)
 
 if __name__ == "__main__":
     main()
